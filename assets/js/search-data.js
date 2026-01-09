1e4ad858a344f36bee95db6b09ffaadc395c8950
Tuesdaythe13th <153651490+Tuesdaythe13th@users.noreply.github.com>
2026-01-09 04:49:49 +0000

    Deploying to gh-pages from @ Tuesdaythe13th/paperwithcode@559d674d8daa7c211faf05c0ac4a1145c5cc3e7f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,9 +37,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/paperwithcode/repositories/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-curriculum-vitae",
+          title: "Curriculum Vitae",
+          description: "A comprehensive record of my academic research, technical contributions, and professional experience in AI Safety and Frontier Systems.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/paperwithcode/cv/";
